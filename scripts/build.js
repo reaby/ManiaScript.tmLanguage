c0824848566e0f3d3d1a8d983162c84231ae3756
Reaby <petri.jarvisalo@gmail.com>
2019-12-11 02:51:55 +0200

    codestyle fixes

--- scripts/build.js
+++ scripts/build.js
@@ -2,39 +2,36 @@
 
 require('events').EventEmitter.prototype._maxListeners = 100;
 
-const fs = require('fs')
-const YAML = require('yamljs')
-const chalk = require('chalk')
-const plist = require('plist')
-const yamlLoader = require('./yaml-import-loader')
+const fs = require('fs'), YAML = require('yamljs'), chalk = require('chalk'), plist = require('plist'),
+    yamlLoader = require('./yaml-import-loader');
 
 yamlLoader('src/syntaxes/_index.yaml', (err, res) => {
-  if (err) {
-	  console.log(err);
-	  throw new Error();
-  }
-  const encodeObject = (obj) => {
-    const str = JSON.stringify(obj)
-    const esc = str
-      .replace(/&/g, '&amp;')
-      .replace(/\</g, '&lt;')
-      .replace(/\>/g, '&gt;')
-      .replace(/'/g, '&apos;')    
+    if (err) {
+        console.log(err);
+        throw new Error();
+    }
+    const encodeObject = (obj) => {
+        const str = JSON.stringify(obj);
+        const esc = str
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/'/g, '&apos;');
 
-    return JSON.parse(esc)
-  }
+        return JSON.parse(esc)
+    };
 
-  const yamlString = res.buffer
-  const pojo = encodeObject(YAML.parse(yamlString))
-  const jsonString = JSON.stringify(YAML.parse(yamlString), null, 4)
-  const plistString = plist.build(pojo)
-  const builds = [
-    ['build/syntaxes/ManiaScript.YAML-tmLanguage', yamlString],
-    ['build/syntaxes/ManiaScript.json', jsonString],
-    ['build/syntaxes/ManiaScript.tmLanguage', plistString]
-  ]
+    const yamlString = res.buffer;
+    const pojo = encodeObject(YAML.parse(yamlString));
+    const jsonString = JSON.stringify(YAML.parse(yamlString), null, 4);
+    const plistString = plist.build(pojo);
+    const builds = [
+        ['build/syntaxes/ManiaScript.YAML-tmLanguage', yamlString],
+        ['build/syntaxes/ManiaScript.json', jsonString],
+        ['build/syntaxes/ManiaScript.tmLanguage', plistString]
+    ];
 
-  builds.forEach(([path, buffer]) => 
-    fs.writeFileSync(path, buffer, 'utf8', 
-      console.log(chalk.yellow('write'), chalk.green(`${path}... done!`))))
-})
+    builds.forEach(([path, buffer]) =>
+        fs.writeFileSync(path, buffer, 'utf8',
+            console.log(chalk.yellow('write'), chalk.green(`${path}... done!`))))
+});
--- scripts/yaml-import-loader.js
+++ scripts/yaml-import-loader.js
@@ -1,37 +1,37 @@
-const {resolve: resolvePath} = require('path')
-const {createReadStream} = require('fs')
-const assert = require('assert')
-const chalk = require('chalk')
-const split = require('split')
+const {resolve: resolvePath} = require('path');
+const {createReadStream} = require('fs');
+const assert = require('assert');
+const chalk = require('chalk');
+const split = require('split');
 const PadStream = require('pad-stream');
-const ConcatStream = require('concat-stream')
+const ConcatStream = require('concat-stream');
 
-const projectPath = (path) => resolvePath(process.cwd(), path)
+const projectPath = (path) => resolvePath(process.cwd(), path);
 
 function loader(fileName, callback) {
-    assert(fileName, 'fileName must exist')
-    assert(callback && typeof callback === 'function', 'callback(err, res) must be provided')
+    assert(fileName, 'fileName must exist');
+    assert(callback && typeof callback === 'function', 'callback(err, res) must be provided');
     const filePath = projectPath(fileName);
-    const relPath = filePath.split('/').slice(0, -1).join('/')
+    const relPath = filePath.split('/').slice(0, -1).join('/');
     const lineStream = createReadStream(filePath).pipe(split());
     const concatStream = ConcatStream((result) => {
         callback(null, {stream: concatStream, buffer: result})
-    })
-    concatStream.on('error', callback)
+    });
+    concatStream.on('error', callback);
     lineStream.on('data', (data) => {
-        const regex = /^\s*#import\('(.*)'\)/g
+        const regex = /^\s*#import\('(.*)'\)/g;
         const match = regex.exec(data);
         if (match) {
-            const srcPath = resolvePath(relPath, match[1]) + '.YAML-tmLanguage'
-            const srcDir = srcPath.split('/').slice(0, -1).join('/')
-            const importFile = '/' + srcPath.split('/').pop()
-            console.log(chalk.cyan('#import'), chalk.grey(srcDir) + chalk.magenta(importFile))
+            const srcPath = resolvePath(relPath, match[1]) + '.YAML-tmLanguage';
+            const srcDir = srcPath.split('/').slice(0, -1).join('/');
+            const importFile = '/' + srcPath.split('/').pop();
+            console.log(chalk.cyan('#import'), chalk.grey(srcDir) + chalk.magenta(importFile));
 
-            const importStream = createReadStream(srcPath)
-            const padding = data.match(/^\s*/)[0]
-            const padStream = PadStream(4, padding[0])
+            const importStream = createReadStream(srcPath);
+            const padding = data.match(/^\s*/)[0];
+            const padStream = PadStream(4, padding[0]);
 
-            padStream.on('error', callback)
+            padStream.on('error', callback);
             importStream.pipe(padStream).pipe(concatStream)
 
         } else {
@@ -43,6 +43,6 @@ function loader(fileName, callback) {
 function YAML() {
 }
 
-YAML.Import = {loader}
+YAML.Import = {loader};
 
 module.exports = YAML.Import.loader;
